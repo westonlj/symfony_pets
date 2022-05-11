3470e6fb36f25a300ee33df8a2e18cda1d72bce5
Liam Weston <lweston@ncees.org>
2022-05-11 12:59:19 -0400

    First commit
    Created repo and pushing to show additional changes to the files of the practice code.

---
+++ lib/functions.php
@@ -0,0 +1,15 @@
+<?php
+
+function get_pets()
+{
+    $petsJson = file_get_contents('data/pets.json');
+    $pets = json_decode($petsJson, true);
+
+    return $pets;
+}
+
+function save_pets($petsToSave)
+{
+    $json = json_encode($petsToSave, JSON_PRETTY_PRINT);
+    file_put_contents('data/pets.json', $json);
+}
